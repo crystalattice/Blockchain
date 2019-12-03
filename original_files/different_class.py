eeee8ffdd8438caa01a150c2e8a9622d4984f4c4
codyjackson <crystalattice@gmail.com>
2019-12-03 15:11:07 -0600

    Modified and cleaned up different_class.py Started adding modbus-specific code

--- original_files/different_class.py
+++ original_files/different_class.py
@@ -1,15 +1,15 @@
 import time
 import hashlib
+import pickle
 
 
 class Block(object):
-
-    def __init__(self, index, proof, previous_hash, transactions, timestamp=None):
+    def __init__(self, index, new_proof, previous_hash, transactions, timestamp=time.time()):
         self.index = index
-        self.proof = proof
+        self.proof = new_proof
         self.previous_hash = previous_hash
         self.transactions = transactions
-        self.timestamp = timestamp or time.time()
+        self.timestamp = timestamp
 
     @property
     def get_block_hash(self):
@@ -23,7 +23,6 @@ class Block(object):
 
 
 class BlockChain(object):
-
     def __init__(self):
         self.chain = []
         self.current_node_transactions = []
@@ -32,45 +31,44 @@ class BlockChain(object):
 
     @property
     def get_serialized_chain(self):
-        return [vars(block) for block in self.chain]
+        return [vars(each_block) for each_block in self.chain]
+
+    @staticmethod
+    def pickle_cmd(cmd):
+        """Serialize modbus command for JSON object."""
+        modbus_pickle = cmd.pickle()
+
+        return modbus_pickle
 
     def create_genesis_block(self):
-        self.create_new_block(proof=0, previous_hash=0)
+        self.create_new_block(block_proof=0, previous_hash=0)
 
-    def create_new_block(self, proof, previous_hash):
-        block = Block(
+    def create_new_block(self, block_proof, previous_hash):
+        new_block = Block(
             index=len(self.chain),
-            proof=proof,
+            new_proof=block_proof,
             previous_hash=previous_hash,
             transactions=self.current_node_transactions
         )
         self.current_node_transactions = []  # Reset the transaction list
 
-        self.chain.append(block)
-        return block
+        self.chain.append(new_block)
+        return new_block
 
     @staticmethod
     def is_valid_block(block, previous_block):
         if previous_block.index + 1 != block.index:
             return False
-
         elif previous_block.get_block_hash != block.previous_hash:
             return False
-
         elif not BlockChain.is_valid_proof(block.proof, previous_block.proof):
             return False
-
         elif block.timestamp <= previous_block.timestamp:
             return False
-
         return True
 
     def create_new_transaction(self, sender, recipient, amount):
-        self.current_node_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount
-        })
+        self.current_node_transactions.append(dict(sender=sender, recipient=recipient, amount=amount))
         return True
 
     @staticmethod
@@ -86,15 +84,15 @@ class BlockChain(object):
         A very simple `Proof of Work` Algorithm -
             - Find a number such that, sum of the number and previous POW number is divisible by 7
         """
-        proof = previous_proof + 1
-        while not BlockChain.is_valid_proof(proof, previous_proof):
-            proof += 1
+        new_proof = previous_proof + 1
+        while not BlockChain.is_valid_proof(new_proof, previous_proof):
+            new_proof += 1
 
-        return proof
+        return new_proof
 
     @staticmethod
-    def is_valid_proof(proof, previous_proof):
-        return (proof + previous_proof) % 7 == 0
+    def is_valid_proof(new_proof, previous_proof):
+        return (new_proof + previous_proof) % 7 == 0
 
     @property
     def get_last_block(self):
@@ -108,35 +106,27 @@ class BlockChain(object):
         current_index = 1
 
         while current_index < len(self.chain):
-
-            block = self.chain[current_index]
-
-            if not self.is_valid_block(block, previous_block):
+            current_block = self.chain[current_index]
+            if not self.is_valid_block(current_block, previous_block):
                 return False
-
-            previous_block = block
+            previous_block = current_block
             current_index += 1
-
         return True
 
     def mine_block(self, miner_address):
         # Sender "0" means that this node has mined a new block
-        # For mining the Block(or finding the proof), we must be awarded with some amount(in our case this is 1)
-        self.create_new_transaction(
-            sender="0",
-            recipient=miner_address,
-            amount=1,
-        )
+        # For mining the Block(or finding the new_proof), we must be awarded with some amount(in our case this is 1)
+        self.create_new_transaction(sender="0", recipient=miner_address, amount=1)
 
-        last_block = self.get_last_block
+        previous_block = self.get_last_block
 
-        last_proof = last_block.proof
-        proof = self.create_proof_of_work(last_proof)
+        previous_proof = previous_block.proof
+        new_proof = self.create_proof_of_work(previous_proof)
 
-        last_hash = last_block.get_block_hash
-        block = self.create_new_block(proof, last_hash)
+        previous_hash = previous_block.get_block_hash
+        new_block = self.create_new_block(new_proof, previous_hash)
 
-        return vars(block)  # Return a native Dict type object
+        return vars(new_block)  # Return a native Dict type object
 
     def create_node(self, address):
         self.nodes.add(address)
@@ -154,27 +144,25 @@ class BlockChain(object):
 
 
 if __name__ == "__main__":
-    blockchain = BlockChain()
+    block_chain = BlockChain()
 
     print(">>>>> Before Mining...")
-    print(blockchain.chain)
+    print(block_chain.chain)
 
-    last_block = blockchain.get_last_block
+    last_block = block_chain.get_last_block
     last_proof = last_block.proof
-    proof = blockchain.create_proof_of_work(last_proof)
+    proof = block_chain.create_proof_of_work(last_proof)
 
     # Sender "0" means that this node has mined a new block
     # For mining the Block(or finding the proof), we must be awarded with some amount(in our case this is 1)
-
-    blockchain.create_new_transaction(
+    block_chain.create_new_transaction(
         sender="0",
         recipient="address_x",
         amount=1,
     )
-
     last_hash = last_block.get_block_hash
-    block = blockchain.create_new_block(proof, last_hash)
+    block = block_chain.create_new_block(proof, last_hash)
 
     print(">>>>> After Mining...")
-    for blocks in blockchain.chain:
+    for blocks in block_chain.chain:
         print(blocks)
